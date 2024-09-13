56353c605b10b1bb266bd4c46228a16f79abadb6
Himanshu Goyal <himanshugoyal1011@gmail.com>
2024-09-13 20:22:27 +0530

    quick fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         
         <ToastContainer
           position="top-right"
-          autoClose={5000}
+          autoClose={1000}
           hideProgressBar={false}
           newestOnTop
           closeOnClick
